e36dc335dd623e32a472edf9a78c086c5c1cda83
Psarmmiey <psarmmiey@gmail.com>
2016-11-18 21:07:43 +0100

    Overridden ArrayAdapter method getView

--- app/src/main/java/com/psarmmiey/weatherviewer/WeatherArrayAdapter.java
+++ app/src/main/java/com/psarmmiey/weatherviewer/WeatherArrayAdapter.java
@@ -2,6 +2,9 @@ package com.psarmmiey.weatherviewer;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -31,5 +34,63 @@ class WeatherArrayAdapter extends ArrayAdapter<Weather> {
     // constructor to initialize inherited member
     public WeatherArrayAdapter(Context context, List<Weather> forecast) {
         super (context, -1, forecast);
+    }
+
+    // creates the custom views for the ListView's items
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // get weather object for this specified ListView position
+        Weather day = getItem(position);
+
+        ViewHolder viewHolder; // object that reference's list item's views
+
+        // check for reusable ViewHolder from a ListView item that scrolled
+        // offscreen; otherwise, create a new ViewHolder
+        if (convertView == null) { // no reusable ViewHolder, so create one
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView =
+                    inflater.inflate(R.layout.list_item, parent, false);
+            viewHolder.conditionImageView =
+                    (ImageView) convertView.findViewById(R.id.conditionImageView);
+            viewHolder.dayTextView =
+                    (TextView) convertView.findViewById(R.id.dayTextView);
+            viewHolder.lowTextView =
+                    (TextView) convertView.findViewById(R.id.lowTextView);
+            viewHolder.hiTextView =
+                    (TextView) convertView.findViewById(R.id.hiTextView);
+            viewHolder.humidityTextView =
+                    (TextView) convertView.findViewById(R.id.humidityTextView);
+            convertView.setTag(viewHolder);
+
+        }
+        else { // reuse existing ViewHolder stored as the list item's tag
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        // if weather condition icon already downloaded, use it;
+        // otherwie, download icon in a separate thread
+        // download and display weather condition image
+        if (bitmaps.containsKey(day.iconURL)) {
+            viewHolder.conditionImageView.setImageBitmap(
+                    bitmaps.get(day.iconURL)
+            );
+        }
+        else new LoadImageTask(viewHolder.conditionImageView).execute(
+                day.iconURL);
+
+        // get other data from Weather object and place into views
+        Context context = getContext(); // for loading String resources
+        viewHolder.dayTextView.setText(context.getString(
+                R.string.day_description, day.dayOfWeek, day.description));
+        viewHolder.lowTextView.setText(
+                context.getString(R.string.low_temp, day.minTemp));
+        viewHolder.hiTextView.setText(
+                context.getString(R.string.high_temp, day.maxTemp));
+        viewHolder.humidityTextView.setText(
+                context.getString(R.string.humidity, day.humidity));
+
+        return convertView; // return completed list item to display
+
     }
  }
